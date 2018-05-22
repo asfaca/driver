9d424d3c5f7de0d74a05be33dd52b22aacaf6e29
swkim <swkim@localhost.localdomain>
2018-05-22 21:28:23 +0900

    lld edition3 ch3 64p, copy_to_user, copy_from_user, 05.22.18

--- main.c
+++ main.c
@@ -5,18 +5,103 @@
 #include <linux/fs.h>
 #include <linux/kdev_t.h>
 #include <linux/cdev.h>
+#include <linux/slab.h>
 
 #include "scull.h" 
 
 int scull_major = SCULL_MAJOR;
 int scull_minor = 0;
 int scull_nr_devs = SCULL_NR_DEVS;
+int scull_quantum = SCULL_QUANTUM;
+int scull_qset = SCULL_QSET;
+
+struct scull_dev *scull_devices;
+
+ssize_t scull_read(struct file *filp, char __user *buff,
+    size_t count, loff_t *offp)
+{
+    
+}
+
+ssize_t scull_write(struct file *filp, char __user *buff,
+    size_t count, loff_t *offp)
+{
+    
+}
+
+/* 
+ * scull_trim is in charge of freeing the whole data
+ * area and is invoked by scull_open when the file is
+ * opened for writing
+ */
+int scull_trim(struct scull_dev *dev)
+{
+    struct scull_qset *next, *dptr;
+    int qset = dev->qset;
+    int i;
+
+    for (dptr = dev->data; dptr; dptr = next) {
+        if (dptr->data) {
+            for (i = 0; i < qset; i++)
+                kfree(dptr->data[i]);
+            kfree(dptr->data);
+            dptr->data = NULL;
+        }
+        next = dptr->next;
+        kfree(dptr);
+    }
+    dev->size = 0;
+    dev->quantum = scull_quantum;
+    dev->qset = scull_qset;
+    dev->data = NULL;
+    return 0;
+}
+
+int scull_open(struct inode *inode, struct file *filp)
+{
+    /* identify which device is being opened */
+    struct scull_dev *dev;
+
+    dev = container_of(inode->i_cdev, struct scull_dev, cdev);
+    filp->private_data = dev; /* for other method */
+
+    /* trim to 0 the length of the device if open was write only */
+}
+
+int scull_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+struct file_operations scull_fops = {
+    .owner =    THIS_MODULE,
+    .llseek =   scull_llseek,
+    .read =     scull_read,
+    .write =    scull_write,
+    .ioctl =    scull_ioctl,
+    .open =     scull_open,
+    .release =  scull_release,       
+};
+
+static void scull_setup_cdev(struct scull_dev *dev, int index)
+{
+    int err, devno = MKDEV(scull_major, scull_minor + index);
+
+    cdev_init(&dev->cdev, &scull_fops);
+    dev->cdev.owner = THIS_MODULE;
+    dev->cdev.ops = &scull_fops;
+    err = cdev_add(&dev->cdev, devno, 1);
+
+    if (err)
+        printk(KERN_NOTICE "Error : %d adding scull%d", err, index);
+}
 
 static int scull_init(void)
 {
     dev_t dev;
     int result;
 
+    /* Step1 : allocate device number (major number and minor number) */
     if (scull_major) {
         dev = MKDEV(scull_major, scull_minor);
         result = register_chrdev_region(dev, scull_nr_devs, "scull");
@@ -29,6 +114,10 @@ static int scull_init(void)
         printk(KERN_WARNING "scull: can't get major");
         return result;
     }
+
+    /* Step2 : allocate and initilize device structure*/
+
+    /* Step3 : allocate cdev structure and register device structure to kernel */
 
     return 0;
 }
--- scull.h
+++ scull.h
@@ -1,2 +1,20 @@
 #define SCULL_MAJOR 0
 #define SCULL_NR_DEVS 4
+#define SCULL_QUANTUM
+#define SCULL_QSET
+
+
+struct scull_qset {
+        void **data;    /* data array (array of quantum) */
+        struct scull_qset *next;
+}
+
+struct scull_dev {
+        struct scull_qset *data;  /* Pointer ro first quantum set */
+        int quantum;              /* the current quantum size */
+        int qset;                 /* the current quantum array size */
+        unsigned long size;       /* amount of data stored here */
+        unsigned int access_key   /* used by sculluid and scullpriv */
+        struct semaphore sem;     /* mutual exclusion semaphore */
+        struct cdev cdev;         /* Char device structure */
+}
